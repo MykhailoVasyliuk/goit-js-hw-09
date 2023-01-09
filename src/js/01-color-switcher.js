774af84ff7cd1e8f08015c33e063d4782581df04
MykhailoVasyliuk <110117416+MykhailoVasyliuk@users.noreply.github.com>
2023-01-09 18:07:50 +0200

    completed task - 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const bodyRef = document.querySelector("body");
+const startBtnRef = document.querySelector("button[data-start]");
+const stopBtnRef = document.querySelector("button[data-stop]");
+ 
+startBtnRef.addEventListener('click', onChangeColorStart);
+stopBtnRef.addEventListener('click', onChangeColorStop);
 
+let changeColorTimerId = null;
+
+function onChangeColorStart(evt) {
+    onBtnDisabled();
+    changeColorTimerId = setInterval(() => {
+        bodyRef.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function onChangeColorStop(evt) {
+    onBtnDisabled();
+    clearTimeout(changeColorTimerId);
+}
+
+function onBtnDisabled() {
+    if (!startBtnRef.disabled) {
+        startBtnRef.disabled = true;
+        stopBtnRef.disabled = false;
+    } else {
+        startBtnRef.disabled = false;
+        stopBtnRef.disabled = true;
+    }
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
